ce5a83b28765d3b31f6aec5330b48cef13b2cd8d
sabrababra <sabrinayesminprome@gmail.com>
2022-10-25 23:09:59 +0600

    create all courses and single couse rout

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const express=require('express')
-const app=express();
-const port= process.env.PORT || 5000;
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
 
-const courses=require('./data/cousrses.json');
+const courses = require('./data/cousrses.json');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('course api running');
 })
-
-app.get('/courses',(req,res)=>{
+//all course api
+app.get('/courses', (req, res) => {
     res.send(courses);
 })
-app.get('/courses',(req,res)=>{
-    res.send(courses);
+// single course api
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    const course = courses.find(c => c.id === id);
+    console.log(course);
+    res.send(course);
 })
 
-app.listen(port,()=>{
+// app.get('/courses/:id', (req, res) => {
+//     console.log(courses);
+//     const id = req.params.id;
+//     const course = courses.find(n => n._id === id);
+//     console.log(course);
+//     res.send(course);
+// });
+
+app.listen(port, () => {
     console.log('course api running on port');
 })
